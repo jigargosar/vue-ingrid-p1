cc0b76dda3d52d7633f06127af9259502090045e
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-22 18:06:15 +0530

    styling

---
+++ src/components/NodeTree.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NodeTree',
+  props: {
+    title: String,
+    forest: Array,
+  },
+}
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="tl">
     {{ title }}
+    <NodeTree :title="root.title" :forest="root.forest" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import NodeTree from '../components/NodeTree'
+
 export default {
   name: 'home',
-  components: {},
+  components: { NodeTree },
   data: () => ({
     title: 'HW',
     root: { title: 'Root', forest: [{ title: 'Node 1' }] },
