fd90504f5b7f07b7157fb998c4a85279e6c001ae
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-22 17:56:57 +0530

    styling

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,6 @@
 </template>
 
 <style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-}
 #app {
   /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
